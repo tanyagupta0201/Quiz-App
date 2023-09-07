5ab48a8e677f099f44f550ed98d205e27e5a489d
Rajit Deb <rajitdeb456@gmail.com>
2023-09-07 13:28:37 +0530

    Added code to selected_question_option.xml, updated gradle dependencies and API Level, added new strings and used appropriate padding for the screens, button sizing

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,29 +1,44 @@
 package com.example.quizapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        button.setOnClickListener{
-            if(input.text.toString().isEmpty())
-            {
-                Toast.makeText(this, "Enter Your Name", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                var intent = Intent(this, QuestionActivity::class.java)
-                intent.putExtra("${setData.name}", input.text.toString())
+        binding.button.setOnClickListener {
+
+            val name = binding.input.editText?.text.toString().trimEnd()
+
+            if(name.isNotEmpty()) {
+
+                val intent = Intent(this@MainActivity, QuestionActivity::class.java)
+                intent.putExtra("${SetData.name}", name)
                 startActivity(intent)
                 finish()
+
+            } else {
+
+                Toast.makeText(
+                    this@MainActivity,
+                    "Please enter your name",
+                    Toast.LENGTH_SHORT
+                ).show()
+
             }
         }
+
     }
 }
--- app/src/main/java/com/example/quizapp/SetData.kt
+++ app/src/main/java/com/example/quizapp/SetData.kt
@@ -1,13 +1,15 @@
 package com.example.quizapp
 
-object setData {
+object SetData {
 
     const val name: String = "name"
     const val score: String = "score"
 
      fun getQuestion():ArrayList<QuestionData>{
-         var que: ArrayList<QuestionData> = arrayListOf()
-         var question1 = QuestionData(
+
+         val que: ArrayList<QuestionData> = arrayListOf()
+
+         val question1 = QuestionData(
              "What is the capital of India ?",
              1,
              "Mumbai",
@@ -16,7 +18,8 @@ object setData {
              "Kolkata",
              3
          )
-         var question2 = QuestionData(
+
+         val question2 = QuestionData(
              "Who is the first Prime Minister of India ?",
              2,
              "Narendra Modi",
@@ -25,7 +28,8 @@ object setData {
              "Jawahar Lal Nehru",
              4
          )
-         var question3 = QuestionData(
+
+         val question3 = QuestionData(
              "What is the name of Sholay' s iconic villain ?",
              3,
              "Kaalia",
@@ -34,7 +38,8 @@ object setData {
              "Gabbar Singh",
              4
          )
-         var question4 = QuestionData(
+
+         val question4 = QuestionData(
              "Which of the following is not a junk food ?",
              4,
              "Pizza",
@@ -43,7 +48,8 @@ object setData {
              "Momos",
              3
          )
-         var question5 = QuestionData(
+
+         val question5 = QuestionData(
              "How many days do we have in a week ?",
              5,
              "Seven",
@@ -52,11 +58,13 @@ object setData {
              "Six",
              1
          )
+
          que.add(question1)
          que.add(question2)
          que.add(question3)
          que.add(question4)
          que.add(question5)
+
          return que
      }
 }
